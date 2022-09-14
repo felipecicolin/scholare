695b92c8aaac5c505b0c63d8f8ec39ac092c4bfb
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-09-14 20:02:57 -0300

    Commit

--- spec/models/alternative_spec.rb
+++ spec/models/alternative_spec.rb
@@ -2,10 +2,21 @@ RSpec.describe Alternative do
   describe "validations" do
     describe "option" do
       it { is_expected.to validate_presence_of(:option) }
+      it { is_expected.to allow_value("A").for(:option) }
+      it { is_expected.to allow_value("B").for(:option) }
+      it { is_expected.to allow_value("C").for(:option) }
+      it { is_expected.to allow_value("D").for(:option) }
+      it { is_expected.to allow_value("E").for(:option) }
+      it { is_expected.not_to allow_value("F").for(:option) }
     end
 
     describe "body" do
       it { is_expected.to validate_presence_of(:body) }
+    end
+
+    describe "correct" do
+      it { is_expected.to allow_value(true).for(:correct) }
+      it { is_expected.to allow_value(false).for(:correct) }
     end
   end
 
