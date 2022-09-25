163c5e803174ab59b358ab69c82eade687905a0f
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-09-25 12:50:59 -0300

    Commit

--- app/components/school_classes/form/component.rb
+++ app/components/school_classes/form/component.rb
@@ -3,11 +3,11 @@
 module SchoolClasses
   module Form
     class Component < ViewComponent::Base
-      def initialize(school_class:, method:, url:, submit_text:, current_user:)
+      def initialize(school_class:, method:, url:, submit_button_text:, current_user:)
         @school_class = school_class
         @url = url
         @method = method
-        @submit_text = submit_text
+        @submit_button_text = submit_button_text
         @current_user = current_user
       end
     end
