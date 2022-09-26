136613959aa8f6f3fd1800937d840065ff86e824
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-09-25 23:04:26 -0300

    Commit

--- app/components/form/component.rb
+++ app/components/form/component.rb
@@ -2,11 +2,10 @@
 
 module Form
   class Component < ViewComponent::Base
-    def initialize(resource:, form_fields:, method:, url:)
+    def initialize(resource:, form_fields:, url:)
       @resource = resource
       @form_fields = form_fields
       @url = url
-      @method = method
     end
 
     private
@@ -21,6 +20,18 @@ module Form
 
     def edit_text
       I18n.t("shared.buttons.edit") if @resource.persisted?
+    end
+
+    def form_method
+      patch_method || post_method
+    end
+
+    def patch_method
+      :patch if @resource.persisted?
+    end
+
+    def post_method
+      :post if @resource.new_record?
     end
   end
 end
