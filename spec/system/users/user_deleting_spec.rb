db5989a5656cd64ca22eb01ad4e64953164383d3
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-09-29 19:27:40 -0300

    Commit

--- spec/system/users/user_deleting_spec.rb
+++ spec/system/users/user_deleting_spec.rb
@@ -1,12 +1,8 @@
 RSpec.describe "User Deleting" do
-  let(:user) { create(:user) }
-
-  before do
-    sign_in user
-    visit edit_user_registration_path
-  end
-
   it "successfully deletes the user's account" do
+    sign_in create(:user)
+    visit edit_user_registration_path
+
     click_button "Cancelar minha conta"
 
     expect(page).to have_content("A sua conta foi cancelada com sucesso.")
