5f205bcd84b20258767fc1df49f196f76cc677ac
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-09-28 21:37:52 -0300

    Commit

--- spec/system/students/students_crud_spec.rb
+++ spec/system/students/students_crud_spec.rb
@@ -80,7 +80,7 @@ RSpec.describe "Students CRUD" do
     end
   end
 
-  describe "Students filtering" do
+  describe "Students filtering", js: true do
     it "successfully filters Students by School Class" do
       create(:student, user:, name: "First Student", school_class: first_class)
       create(:student, user:, name: "Second Student", school_class: second_class)
