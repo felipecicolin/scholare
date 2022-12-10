00b89dcf019373bead18146229078044e055ddb5
Felipe Cicolin Rocha <felipecicolinrocha@gmail.com>
2022-12-10 15:24:12 -0300

    Commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each do |f|
 end
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = Rails.root.join("spec/fixtures")
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
