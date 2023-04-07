171192926e9d7dfb8790e2b7d669279d03a2ea52
Felipe Cicolin <felipecicolinrocha@gmail.com>
2023-04-07 00:43:17 -0300

    Adds Turbo (#169)

--- spec/system/nav_spec.rb
+++ spec/system/nav_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "Nav" do
       click_button user.email
 
       expect(page).to have_link("Editar conta", href: edit_user_registration_path)
-      expect(page).to have_button("Sair")
+      expect(page).to have_link("Sair", href: destroy_user_session_path)
     end
   end
 end
